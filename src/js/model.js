27dba1961014621da5fd0b8a33553a3e88aa2a80
andzia0520 <ankadaniluk@gmail.com>
2023-09-13 11:33:38 +0200

    implementation of MVC architecture, adding comfig and helpers

---
+++ src/js/model.js
@@ -0,0 +1,30 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+    // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+
+    // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      publisher: recipe.publisher,
+      cookingTime: recipe.cooking_time,
+      servings: recipe.servings,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    console.error(`${err} 😨😨😧😧`);
+  }
+};
