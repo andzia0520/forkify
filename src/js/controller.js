6f9f04d76a9dd513535855ef7c25e5738dbaf6ad
andzia0520 <ankadaniluk@gmail.com>
2023-09-14 14:38:39 +0200

    adding pagination to app

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,9 +5,10 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import resultsView from './views/resultsView';
 import searchView from './views/searchView';
+import paginationView from './views/paginationView';
 
 if (module.hot) {
-  module.hot.accept;
+  module.hot.accept();
 }
 
 const controlRecipes = async function () {
@@ -42,17 +43,28 @@ const controlSearchResults = async function () {
 
     // 3. Render results
     console.log(model.state.search.results);
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
 
     //render initial pagination button
+    paginationView.render(model.state.search);
   } catch (err) {
     recipeView.renderError();
   }
 };
 
+const controlPagination = function (goToPage) {
+  //render NEW result
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  
+  //render initial pagination button
+  console.log(model.state.search);
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 
 init();
