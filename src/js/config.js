96e547d4c9bb0fed16a8af4c72120dd45bbf46fd
andzia0520 <ankadaniluk@gmail.com>
2023-09-22 10:39:16 +0200

    Local storage for bookmarks, adding own recipes

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,7 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 
 export const RES_PER_PAGE = 10;
+
+export const API_KEY = '83afcb48-6bc7-4001-bcb3-0d372d4209c6'
+
+export const MODAL_CLOSE_SEC = 2.5;
